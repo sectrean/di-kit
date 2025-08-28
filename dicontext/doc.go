3ca85c13478f9ade10ead54a7f854e7dd2283df4
John Rutherford <sectrean@gmail.com>
2025-08-28 19:58:20 -0400

    Add example to dicontext package docs

--- dicontext/doc.go
+++ dicontext/doc.go
@@ -1,4 +1,22 @@
 /*
-Package dicontext provides utilities for storing [di.Scope] on [context.Context].
+Package dicontext provides utilities for working with [di.Scope] and [context.Context].
+
+Example:
+
+	c, err := di.NewContainer(DependencyModule)
+	...
+
+	// Add the Container to the context
+	ctx := context.Background()
+	ctx = dicontext.WithScope(ctx, c)
+	...
+
+	// Resolve dependencies using the container scope on the context
+	svc, err := dicontext.Resolve[MyService](ctx)
+	...
 */
 package dicontext
+
+import "github.com/sectrean/di-kit"
+
+var _ di.Scope = nil
